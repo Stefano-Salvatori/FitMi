73c0c4f673242e804c1a3076db7b67f8e065cf53
Stefano Salvatori <stefano.salvatori5@studio.unibo.it>
2019-07-29 01:17:17 +0200

    Graphic Improvements; Added Login and Register Logic
    - GUI improvements for the login and sign-in page
    - Added logic to handle login requests
    - Added logic to add users in the database from the sign-in page
    - Changed Ionic default colors to match the applicatoin theme (see: https://ionicframework.com/docs/theming/color-generator)
    - Added packages to deploy the application on android devices

--- fitmi/src/app/home/home.component.ts
+++ fitmi/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-home',
@@ -10,14 +12,30 @@ export class HomeComponent implements OnInit {
 
   private registrationRoute: string = "sign-in";
   private loginRoute: string = "login";
-  private title: string = "FIT MI";
+  private logoImage: string = "assets/icon/logo_icon.png";
 
-  constructor(private router: Router) { }
+  private username: String;
+  private password: String;
+
+
+  constructor(private router: Router, private httpClient: HttpClient) { }
 
   ngOnInit() {}
 
   login() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })
+    };
+    console.log(this.username);
+    console.log(this.password);
 
+    this.httpClient.post("http://192.168.1.4:3000/users/login",{
+      username: this.username, 
+      password: this.password
+    }, httpOptions)
+      .subscribe(data => console.log(data));
   }
 
   register() {
--- fitmi/src/app/sign-in/sign-in.component.ts
+++ fitmi/src/app/sign-in/sign-in.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClientService } from '../http-client.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-sign-in',
@@ -8,29 +11,32 @@ import { HttpClientService } from '../http-client.service';
 })
 export class SignInComponent implements OnInit {
 
-  private title: string = "Registrati";
-  private M: string = "M";
+  private logoImage: string = "assets/icon/logo_icon.png";
+  /*private M: string = "M";
   private F: string = "F";
 
   private username: string;
   private password: string;
   private birthDate: string;
   private gender: string;
-  private heigth: number;
-  private weight: number;
+  private height: number;
+  private weight: number;*/
 
 
-  constructor(private httpClient: HttpClientService) {
-    this.gender = this.M;
+  constructor(private httpClient: HttpClient, 
+    private router: Router) {
+    //this.gender = this.M;
   }
 
   ngOnInit() {}
 
-  register() {
-
+  register(form) {
+    this.httpClient.post("http://192.168.1.4:3000/users", form.value)
+    .subscribe(() => this.router.navigateByUrl("home"))
   }
 
+
   setGender(selectedGender: string) {
-    this.gender = selectedGender;
+    //this.gender = selectedGender;
   }
 }
