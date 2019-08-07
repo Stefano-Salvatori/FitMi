55ecbebc5bd8b963a45446b7684c46b5da32f89c
Gabriele <gabriele.guerrini6@studio.unibo.it>
2019-08-07 03:11:38 +0200

    Add session icons on homepage

--- fitmi/src/app/home/home.component.ts
+++ fitmi/src/app/home/home.component.ts
@@ -10,6 +10,8 @@ export class HomeComponent implements OnInit {
 
   private activities = ['Corsa', 'Camminata', 'Ciclismo', 'Palestra', 'Nuoto', 'Corsa Indoor'];
   private activityRoutes = ['running', 'walking', 'cycling', 'gym', 'swimming', 'indoor-run'];
+  private baseIconPath = "../../assets/img/";
+  private iconExtension = ".png";
 
   constructor(private router: Router) { }
 
@@ -35,6 +37,23 @@ export class HomeComponent implements OnInit {
       case this.activities[5]:
         this.router.navigateByUrl(this.activityRoutes[5]);
         break;
+    }
+  }
+
+  selectImage(activity: string): string {
+    switch(activity) {
+      case this.activities[0]:
+        return this.baseIconPath + this.activityRoutes[0] + this.iconExtension;
+      case this.activities[1]:
+        return this.baseIconPath + this.activityRoutes[1] + this.iconExtension;
+      case this.activities[2]:
+        return this.baseIconPath + this.activityRoutes[2] + this.iconExtension;
+      case this.activities[3]:
+        return this.baseIconPath + this.activityRoutes[3] + this.iconExtension;
+      case this.activities[4]:
+        return this.baseIconPath + this.activityRoutes[4] + this.iconExtension;
+      case this.activities[5]:
+        return this.baseIconPath + this.activityRoutes[5] + this.iconExtension;
     }
   }
 }
