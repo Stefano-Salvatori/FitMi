62171b5105004d4b3618685651d96fc91114eeba
Stefano Salvatori <stefano.salvatori5@studio.unibo.it>
2019-08-22 12:13:55 +0200

    Refactoring Goal class
    - Added eum for GoalType
    - The label and the unit of the goal are now handled inside the enum
    - Now the Goal class takes as parameter a GoalType variable

--- fitmi/src/app/session/goal.ts
+++ fitmi/src/app/session/goal.ts
@@ -1,11 +1,53 @@
+export enum GoalType {
+  STEPS = "Passi",
+  DISTANCE = "Distanza",
+  TIME = "Tempo",
+  CALORIES = "Calorie"
+}
+
+export namespace GoalType {
+  export function getUnit(goalType: GoalType): string {
+    switch (goalType) {
+      case GoalType.STEPS:
+        return "passi";
+      case GoalType.DISTANCE:
+        return "km";
+      case GoalType.TIME:
+        return "min";
+      case GoalType.CALORIES:
+        return "kCal";
+      default:
+        "";
+        break;
+    }
+  }
+}
+
 export class Goal {
 
-  type: string;
-  threshold: number;
-  unit: string;
+  constructor(private _type: GoalType, private _threshold: number) { }
 
-  constructor(type: string, threshold: number) {
-    this.type = type;
-    this.threshold = threshold;
+  get type(): GoalType {
+    return this._type;
+  }
+
+  set type(value: GoalType) {
+    this._type = value;
+  }
+
+  get threshold(): number {
+    return this._threshold;
+  }
+
+  set threshold(value: number) {
+    this._threshold = value;
+  }
+
+  get unit(): string {
+    return GoalType.getUnit(this._type);
+  }
+
+  get label(): string {
+    return GoalType[this._type];
   }
 }
