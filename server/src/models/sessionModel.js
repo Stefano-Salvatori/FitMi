0d01733e0889d721d7a98ff10dcdf28ef4752903
Stefano Salvatori <stefano.salvatori5@studio.unibo.it>
2019-09-01 12:36:16 +0200

    Minor changes in backend mongoose model
    - Added backend support for gps position
    - Added default profile images based on gender

--- server/src/models/sessionModel.js
+++ server/src/models/sessionModel.js
@@ -9,8 +9,22 @@ var SessionSchema = new Schema({
   steps:  { type: Number, default: 0 },
   calories: { type: Number, default: 0 },
   distance:  { type: Number, default: 0 },
-  heart_frequency: [{timestamp: Date,value: Number}]
+  heart_frequency: [{
+    timestamp: Date,
+    value: Number
+  }],
+  gps_path: [{
+    accuracy: { type: Number, default: 0 },
+    altitude: { type: Number, default: 0 },
+    altitudeAccuracy: { type: Number, default: 0 },
+    heading: { type: Number, default: 0 },
+    latitude: { type: Number, default: 0 },
+    longitude: { type: Number, default: 0 },
+    speed: { type: Number, default: 0 },
+  }]
 });
+
+
 
 SessionSchema.methods.totalMins = function () {
   const diffMs = (this.end.getTime() - this.start.getTime());
